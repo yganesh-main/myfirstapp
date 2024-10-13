0edd9d316e5b718e951cd33ce8c4dd26c7cb107b
yganesh-main <yalavarthiganesh358@gmail.com>
2024-10-13 16:13:46 -0400

    Add files via upload

---
+++ ginxapp/script.js
@@ -0,0 +1,7 @@
+document.getElementById('loveButton').addEventListener('click', function() {
+    window.location.href = 'love.html';
+});
+
+document.getElementById('hateButton').addEventListener('click', function() {
+    window.location.href = 'hate.html';
+});
